568a532ed877ac3c568231631baa2121403644a3
Richard Shi <rshi@utexas.edu>
2026-03-24 20:12:21 -0500

    add datamode.py, demo_round_1 file from IMC P2

--- round_0/round_0_v1.py
+++ round_0/round_0_v1.py
@@ -9,5 +9,30 @@ class Product:
     EMERALDS = "EMERALDS"
     TOMATOES = "TOMATOES"
 
+PARAMS = {
+    Product.EMERALDS: {
+        "fair_value": 10000,
+        "take_width": 1,
+        "clear_width": 0,
+    },
+    Product.TOMATOES: {
+        "take_width": 1,
+        "clear_width": 0,
+    },
+}
+
 class Trader:
     
+    def __init__(self, params=None):
+        if params is None:
+            params = PARAMS
+        self.params = params
+
+        self.LIMIT = {Product.EMERALDS: 80, Product.TOMATOES: 80}
+
+    
+    def run(self, state: TradingState):
+        """Only method required. It takes all buy and sell orders for all
+        symbols as an input, and outputs a list of orders to be sent."""
+
+        
